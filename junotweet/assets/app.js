5cc5d9d61ed698592047931693867a51a0299273
guillermo haas-thompson <guillermo@brownbox.me>
2020-09-27 14:39:41 -0400

    Cleanup comments.
    Remove obsolete comments.

--- junotweet/assets/app.js
+++ junotweet/assets/app.js
@@ -52,28 +52,15 @@ $(() => {
   });
 });
 
-// $(function() {
-//   let adder = $('.ml-1').val();
-//   $('.fa-heart').on("click", function() {
-//     adder++;
-//     if (adder >= 1) {
-//       $(this).css('color', 'rgb(220, 53, 69)')
-//     }
-//     $(`.ml-1`).text(adder);
-//   });
-// });
-
 $(function() {
   $('#recentTweets').on('click', '.media .fa-heart', function() {
     const $likes = $(this).find('.ml-1');
-    // let likes = $(this).find('span.ml-1').val();
     let likes = $likes.val()
     console.log(likes)
     likes++;
     if (likes >= 1) {
       $(this).css('color', 'rgb(220, 53, 69)')
     }
-    // $(`.ml-1`).text(likes);
     $likes.val(likes);
     $likes.text($likes.val());
   });
