b74adf80e76bdf841bee606db7118bf3bb75e83a
guillermo haas-thompson <guillermo@brownbox.me>
2020-09-27 12:51:23 -0400

    Tweet > Start to improve UX
    Following code along. If a tweet is empty don't do anything if user presses submit and once tweeted, blank out form.

--- junotweet/assets/app.js
+++ junotweet/assets/app.js
@@ -4,6 +4,7 @@ let userName = '@junobot';
 
 TweeterApp.init = function(){
   console.log(`Hello ${userName}`);
+  $('input[type="submit"]').attr('disabled', true);
 };
 
 $(document).ready(function() {
@@ -31,16 +32,20 @@ $(() => {
   $('#tweet-form').on('submit', function(e) {
     e.preventDefault();
     const newTweet = $('#new-tweet').val();
-    $('#recentTweets')
-      .append(
-        `<div class='media pt-3'>
-          <img src="https://avatars.dicebear.com/api/initials/memoht.svg?r=50&w=64&h=64&backgroundColors[]=brown" class="img-fluid mr-2" alt="${userName}"></img>
-          <p class="media-body pb-3 mb-0 lh-125 border-bottom border-gray">
-            <strong class="d-block ">${userName}</strong>
-            ${newTweet}
-          </p>
-        </div>`
+
+    if (newTweet !== '') {
+      $('#new-tweet').val('');
+      $('#recentTweets')
+        .append(
+          `<div class='media pt-3'>
+            <img src="https://avatars.dicebear.com/api/initials/memoht.svg?r=50&w=64&h=64&backgroundColors[]=brown" class="img-fluid mr-2" alt="${userName}"></img>
+            <p class="media-body pb-3 mb-0 lh-125 border-bottom border-gray">
+              <strong class="d-block ">${userName}</strong>
+              ${newTweet}
+            </p>
+          </div>`
         );
+      }
     console.log(newTweet)
   });
 });
