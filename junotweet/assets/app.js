5fc7757114081886a2d162684f9d47a8b7f42be2
guillermo haas-thompson <guillermo@brownbox.me>
2020-09-27 10:54:41 -0400

    userName > chain more methods to cleanup format
    Add `trim` to remove whitespace and `replace` to remove spaces between.

--- junotweet/assets/app.js
+++ junotweet/assets/app.js
@@ -1,12 +1,12 @@
 const TweeterApp = {};
-const userName = `@` + prompt("Hello. Please enter your username").toLowerCase();
+const userName = `@` + prompt("Hello. Please enter your username").toLowerCase().trim().replace(/\s/g, '');
 
 TweeterApp.init = function(){
   console.log(`Hello ${userName}`);
-  };
+};
 
-  $(document).ready(function() {
-    TweeterApp.init();
-  });
+$(document).ready(function() {
+  TweeterApp.init();
+});
 
 
